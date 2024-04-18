ea398d6cb3eed90cdbebd72c2d133ed41cdbd659
Iverson <iversonwang40@gmail.com>
2024-04-18 13:06:57 +0800

    1. connect to MySQL through sequelize 2. use User model to test connection

---
+++ models/user.js
@@ -0,0 +1,24 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate (models) {
+    }
+  };
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    bio: DataTypes.STRING,
+    photo: DataTypes.STRING,
+    permission: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tableName: 'Users',
+    underscored: true
+  })
+  return User
+}
