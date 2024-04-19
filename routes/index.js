a4fd1839fedbecd34498a39459d0a48dc1aa4c66
Iverson <iversonwang40@gmail.com>
2024-04-19 13:29:17 +0800

    feat: handle error

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const { errorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const router = express.Router()
 const userController = require('../controllers/user-controller')
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 router.post('/signup', userController.signUp)
-
+router.use('/', errorHandler)
 module.exports = router
