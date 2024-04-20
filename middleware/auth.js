b8d2a8681cdff22e8965d7c162d0ef9843e89a1b
Iverson <iversonwang40@gmail.com>
2024-04-20 15:58:23 +0800

    modify: change user login logic

--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,18 @@ const authenticatedAdmin = (req, res, next) => {
   }
 }
 
+const authenticatedUser = (req, res, next) => {
+  if (authHelpers.getUser(req)?.role === 'user') {
+    return next()
+  } else {
+    const error = new Error('無法使用前台')
+    error.statusCode = 403
+    throw error
+  }
+}
+
 module.exports = {
   authLocal,
-  authenticatedAdmin
+  authenticatedAdmin,
+  authenticatedUser
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const { errorHandler } = require('../middleware/error-handler')
-const passport = require('../config/passport')
 const router = express.Router()
 
-const { authLocal, authenticatedAdmin } = require('../middleware/auth')
+const { authLocal, authenticatedAdmin, authenticatedUser } = require('../middleware/auth')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 
 router.post('/admin/signin', authLocal, authenticatedAdmin, userController.signIn)
-router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
+router.post('/signin', authLocal, authenticatedUser, userController.signIn)
 router.post('/signup', userController.signUp)
 
 router.use('/admin', admin)
