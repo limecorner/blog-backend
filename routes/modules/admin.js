3ef378f94a51f4c8cc957f7eaaa399af1c1a7630
Iverson <iversonwang40@gmail.com>
2024-04-22 16:26:08 +0800

    feat: admin delete article

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
+const articleController = require('../../controllers/article-controller')
 
 router.patch('/users/:id', adminController.patchUser)
 
@@ -10,5 +11,7 @@ router.get('/categories', adminController.getCategories)
 router.post('/categories', adminController.postCategory)
 router.put('/categories/:id', adminController.putCategory)
 router.delete('/categories/:id', adminController.deleteCategory)
+
+router.delete('/articles/:id', articleController.deleteArticle)
 
 module.exports = router
