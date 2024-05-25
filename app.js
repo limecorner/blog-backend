421aae35881988968cc6e9cfdbbbc4b47c36d364
Iverson <iversonwang40@gmail.com>
2024-05-25 14:42:29 +0800

    app.listen for develop or deploy

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const passport = require('passport')
 const routes = require('./routes')
 
 const app = express()
-// const PORT = 3000
-const PORT = process.env.PORT || '8080'
 
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
@@ -19,11 +17,16 @@ app.use(methodOverride('_method'))
 
 app.use('/api', routes)
 
-// app.listen(PORT, () => {
-//   console.log(`App is running on http://localhost:${PORT}`)
-// })
-
+// 開發
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = 3000
+  app.listen(PORT, () => {
+    console.log(`App is running on http://localhost:${PORT}`)
+  })
+} else {
 // 部署
-app.listen(PORT, '0.0.0.0', () => {
-  console.log('App is running on https://my-blog-backend.fly.dev')
-})
+  const PORT = process.env.PORT || '8080'
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log('App is running on https://my-blog-backend.fly.dev')
+  })
+}
