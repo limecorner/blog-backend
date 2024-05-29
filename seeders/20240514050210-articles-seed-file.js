025f92ca879caded1ce1c8ab594ff2ffcffba16e
Iverson <iversonwang40@gmail.com>
2024-05-29 19:05:50 +0800

    comment article seed file

--- seeders/20240514050210-articles-seed-file.js
+++ seeders/20240514050210-articles-seed-file.js
@@ -1,26 +1,26 @@
-'use strict'
+// 'use strict'
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const categories = await queryInterface.sequelize.query(
-      'SELECT id FROM Categories;',
-      { type: queryInterface.sequelize.QueryTypes.SELECT }
-    )
+// module.exports = {
+//   up: async (queryInterface, Sequelize) => {
+//     const categories = await queryInterface.sequelize.query(
+//       'SELECT id FROM Categories;',
+//       { type: queryInterface.sequelize.QueryTypes.SELECT }
+//     )
 
-    await queryInterface.bulkInsert('Articles',
-      Array.from({ length: 10 }, (_, i) => ({
-        user_id: 36,
-        category_id: categories[Math.floor(Math.random() * categories.length)].id,
-        title: `${i}QQQQQQQQQ`,
-        permission: 'login',
-        content: `${i}WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW`,
-        created_at: new Date(),
-        updated_at: new Date()
-      }))
-    )
-  },
+//     await queryInterface.bulkInsert('Articles',
+//       Array.from({ length: 10 }, (_, i) => ({
+//         user_id: 36,
+//         category_id: categories[Math.floor(Math.random() * categories.length)].id,
+//         title: `${i}QQQQQQQQQ`,
+//         permission: 'login',
+//         content: `${i}WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW`,
+//         created_at: new Date(),
+//         updated_at: new Date()
+//       }))
+//     )
+//   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Articles', {})
-  }
-}
+//   down: async (queryInterface, Sequelize) => {
+//     await queryInterface.bulkDelete('Articles', {})
+//   }
+// }
